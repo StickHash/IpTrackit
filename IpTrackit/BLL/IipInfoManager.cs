2ea0881793ffc1cf92d9c6c259e084121ced9030
StickHash <chrisb1981@orange.fr>
2021-01-31 16:24:50 +0100

    Building BLL

---
+++ IpTrackit/BLL/IipInfoManager.cs
@@ -0,0 +1,14 @@
+﻿using IpTrackit.BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IpTrackit.BLL
+{
+    interface IIpInfoManager
+    {
+        IpInfo GetIpInfo(string ip);
+    }
+}
---
+++ IpTrackit/BLL/IpInfoManagerImpl.cs
@@ -0,0 +1,17 @@
+﻿using IpTrackit.BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IpTrackit.BLL
+{
+    class IpInfoManagerImpl : IIpInfoManager
+    {
+        public IpInfo GetIpInfo(string ip)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
